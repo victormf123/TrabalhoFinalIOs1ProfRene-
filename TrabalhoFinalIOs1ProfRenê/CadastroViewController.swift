f67e305c3e2b1c4170d708a6cf812a0ac86599e8
victormf123 <victormf.df@hotmail.com>
2018-09-02 18:58:40 -0300

    Projeto de iOs 1 - Feito por Aluno Victor Matheus Freitas portador da matricula: 1631088042

---
+++ TrabalhoFinalIOs1ProfRenê/CadastroViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CadastroViewController.swift
+//  TrabalhoFinalIOs1ProfRenê
+//
+//  Created by Matheus Freitas on 02/09/2018.
+//  Copyright © 2018 Matheus Freitas. All rights reserved.
+//
+import UIKit
+
+class CadastroViewController: UIViewController {
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func exit(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    
+}
